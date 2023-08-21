81215670e8235344b928c59df02285c02f83baeb
Athansya <atoriz98@outlook.com>
2023-08-21 00:08:28 -0600

    Corrected my approach and color coded the output image based on the number of iterations.

--- Tareas/mandelbrot.py
+++ Tareas/mandelbrot.py
@@ -31,15 +31,16 @@ rc("text", usetex=True)
 #                                   CONSTANTS                                  #
 # ---------------------------------------------------------------------------- #
 INITIAL_VALUE = 0 + 0j
-RESOLUTION = 100
+REAL_RESOLUTION = 1000
+IMAG_RESOLUTION = 1000
 # COMPLEX_MESH = np.ogrid[-2: 2: RESOLUTION * 1j, -2: 2: RESOLUTION * 1j]
-REAL = np.linspace(-2, 0.48, RESOLUTION)
-IMAG = np.linspace(-1.12, 1.12, RESOLUTION)
+REAL = np.linspace(-2, 0.48, REAL_RESOLUTION)
+IMAG = np.linspace(-1.12, 1.12, IMAG_RESOLUTION)
 # COMPLEX_MESH = np.meshgrid(X, Y)
 # COMPLEX_MESH = np.ogrid[-2: 2: RESOLUTION * 1j, -2: 2: RESOLUTION * 1j]
-ITERATIONS = 30 
-EPSILON = 0.1
+MAX_ITERATIONS = 30 
 TOWARDS_INF = 4
+COMPLEX_MESH = np.zeros(shape=(len(IMAG), len(REAL))) + MAX_ITERATIONS
 # ---------------------------------------------------------------------------- #
 #                             z_{n + 1} = z_n^2 + c                            #
 
@@ -65,25 +66,22 @@ def mandelbrot_set_equation(current_value: complex, c: complex) -> complex:
 
 def show_mandelbrot_set(real_range: np.ndarray, imag_range: np.ndarray):
     #TODO - FIX THIS FUNCTION. OVERFLOWS FOR SOME REASON
-    # Iterating the mesh
     plt.figure(figsize=(15,10))
-    for real in real_range:
-        for imag in imag_range:
+    for id_i, imag in enumerate(imag_range):
+        for id_r, real in enumerate(real_range):
             # Complex variable
             c = complex(real, imag)
-            count = 0  # Counter
-            current_value = mandelbrot_set_equation(INITIAL_VALUE, c=c)
-            while(count < ITERATIONS and abs(current_value.real + current_value.imag) < TOWARDS_INF):
-                next_value = mandelbrot_set_equation(current_value=current_value, c=c)
-                current_value = next_value
-                count += 1
-            if count == ITERATIONS:
-                plt.scatter(real, imag, marker='x', color='black', s=0.5)
-            else:
-                plt.scatter(real, imag, marker='.', color='blue', s=0.5, alpha=0.3)
+            current_value = INITIAL_VALUE
+            for iteration in range(MAX_ITERATIONS):
+                current_value = mandelbrot_set_equation(current_value=current_value, c=c)
+                if (current_value.real**2 + current_value.imag**2) >= TOWARDS_INF:
+                    COMPLEX_MESH[id_i][id_r] = iteration
+                    break
+            # else:
+                # plt.scatter(real, imag, marker='.', color='blue', s=0.5, alpha=0.3)
 
-    plt.savefig("mandelbrot_test_me.png")
-
+    plt.imshow(COMPLEX_MESH, cmap='viridis')
+    plt.savefig("mandelbrot_test_me_2.png")
 
 
 def run():
@@ -91,16 +89,17 @@ def run():
 
 
 if __name__ == "__main__":
-    # y, x = np.ogrid[-1.4: 1.4: RESOLUTION*1j, -1.8: 1: RESOLUTION*1j]
+    # y, x = np.ogrid[-1.4: 1.4: REAL_RESOLUTION*1j, -1.8: 1: REAL_RESOLUTION*1j]
+    # print(f"{y.shape}")
     # a_array = x + y*1j
     # z_array = np.zeros(a_array.shape)
-    # iterations_till_divergence = ITERATIONS + np.zeros(a_array.shape)
+    # iterations_till_divergence = MAX_ITERATIONS + np.zeros(a_array.shape)
     # print(f"{iterations_till_divergence=}")
     # for h in range(RESOLUTION):
         # for w in range(RESOLUTION):
             # z = z_array[h][w]
             # a = a_array[h][w]
-            # for i in range(ITERATIONS):
+            # for i in range(MAX_ITERATIONS):
                 # z = z**2 + a
                 # if z * np.conj(z) > 4:  # DE DONDE SALE ESE 4!!!!????
                     # iterations_till_divergence[h][w] = i
@@ -109,6 +108,7 @@ if __name__ == "__main__":
     # plt.imshow(iterations_till_divergence, cmap="viridis")
     # plt.savefig("mandelbrot_example.png")
 
+    print(f"{COMPLEX_MESH.shape}")
     show_mandelbrot_set(REAL, IMAG)
     # run()
     # for i in COMPLEX_MESH[0]:
